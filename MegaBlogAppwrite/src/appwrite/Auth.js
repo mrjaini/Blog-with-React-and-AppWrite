ba0216e1e25bffd5ceb00c821bdebb98b10d75f2
mrjaini <aj230074@gmail.com>
2025-09-18 16:10:39 +0530

    Auth implement

---
+++ MegaBlogAppwrite/src/appwrite/Auth.js
@@ -0,0 +1,39 @@
+import config from '../config'
+import {Client , Account , ID} from 'appwrite'
+
+export class AuthService{
+    client = new Client();
+    account;
+    constructor(){
+        this.client
+            .setEndpoint(config.appUrl)
+            .setProject(config.projectId);
+        this.account= new Account(this.client);
+    }
+
+    async createAccount({email , password , name}){
+        try {
+            const userAccount = await this.account.create(ID.unique(), email , password , name);
+            if(userAccount){
+                this.login({email, password})
+            }
+            else{
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email , password}) {
+        try {
+            return await this.account.createEmailPasswordSession({email , password});
+        } catch (error) {
+            throw error;
+        }
+        
+    }
+
+}
+const authService = new AuthService();
+export default authService
--- MegaBlogAppwrite/src/config/config.js
+++ MegaBlogAppwrite/src/config/config.js
@@ -1,8 +1,8 @@
 const config ={
     appUrl:String(import.meta.env.VITE_APP_URL),
-    databaseId : String(import.meta.env.VITE_PROJECT_ID),
-    collectionID : String(import.meta.env.VITE_DATABASE_ID),
-    bucketId : String(import.meta.env.VITE_COLLECTION_ID),
-    projectId : String(import.meta.env.VITE_BUCKET_ID)
+    projectId : String(import.meta.env.VITE_PROJECT_ID),
+    databaseID : String(import.meta.env.VITE_DATABASE_ID),
+    collectionID : String(import.meta.env.VITE_COLLECTION_ID),
+    bucketId : String(import.meta.env.VITE_BUCKET_ID)
 }
 export default config;
