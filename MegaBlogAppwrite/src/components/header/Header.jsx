29e524d35d4d6797b3dd118df2b628bee9b06b56
mrjaini <aj230074@gmail.com>
2025-09-23 21:52:06 +0530

    logout button implement

--- MegaBlogAppwrite/src/components/header/Header.jsx
+++ MegaBlogAppwrite/src/components/header/Header.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { Container , LogoutBtn , Logo } from '../index'
+import {Link , useNavigate} from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import authService from '../../appwrite/Auth'
 
 function Header() {
+    const authStatus = useSelector((state) =>{state.auth.status})
+    const navigate = useNavigate()
+
+    const navItems = [
+        {
+        name:'Home',
+        slug :'/',
+        active: true
+        },
+        {
+        name:'Login',
+        slug :'/login',
+        active: !authStatus
+        },
+        {
+        name:'Signup',
+        slug :'/signup',
+        active: !authStatus
+        },
+        {
+        name:'All Posts',
+        slug :'/all-posts',
+        active: authStatus
+        },
+        {
+        name:'Add Post',
+        slug :'/add-post',
+        active: authStatus
+        },
+     ]
     return (
-        <h1>Header</h1>
+        <header className='py-4 shadow bg-gray-500'>
+
+        </header>
     )
 }
 
